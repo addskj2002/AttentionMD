d28df008eb8deeb07c0891ee8e103bde84c45637
aaronk2002 <aaronakj2002@gmail.com>
2025-10-26 14:32:04 -0400

    Removed unused files and renamed files

--- real-data/bash_scripts/vit_adam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-module load anaconda/2023a-pytorch
-module load cuda/11.8
-
-python3 train_vit.py --optim Adam --filename "out_adam.pt" --lr 1e-4 --epochs 1000
--- real-data/bash_scripts/vit_md.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-module load anaconda/2023a-pytorch
-module load cuda/11.8
-
-python3 train_vit.py --optim SMD --filename "out_md.pt" --lr 5e-2 --epochs 1000
